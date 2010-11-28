32b814287ef48a789e8b8b8a348bdb5f58a05ebe
dave bl <db@d1b.org>
2010-11-28 21:05:54 +1100

    strip the whitespace in the blockfinder test file
    Signed-off-by: dave bl <db@d1b.org>

--- blockfindertest.py
+++ blockfindertest.py
@@ -4,7 +4,7 @@ import unittest
 import os
 
 class CheckReverseLookup(unittest.TestCase):
-     
+
     ipValues = ( (3229318011, '192.123.123.123'),
                     (3463778365, '206.117.16.61'),
                     (4278190202, '255.0.0.122'),
@@ -14,14 +14,14 @@ class CheckReverseLookup(unittest.TestCase):
     rirValues = ( ('217.204.232.15', 'GB'),
                   ('188.72.225.100', 'DE'),
                   ('8.8.8.1', 'US'))
- 
+
     cache_dir = str(os.path.expanduser('~')) + "/.blockfinder/"
 
     def test_rir_lookup(self):
         for ip, cc in self.rirValues:
             result = blockfinder.rir_lookup(ip, self.cache_dir)
-            self.assertEqual(result[0], cc) 
-        
+            self.assertEqual(result[0], cc)
+
     def test_ip_address_to_dec(self):
         for dec, ip in self.ipValues:
             result = blockfinder.ip_address_to_dec(ip)
@@ -29,11 +29,11 @@ class CheckReverseLookup(unittest.TestCase):
 
 class CheckBlockFinder(unittest.TestCase):
     cache_dir = str(os.path.expanduser('~')) + "/.blockfinder/"
-    
-   
+
+
     # You can add known blocks to the tuple as a list
     # they will be looked up and checked
-    knownResults = ( ('mm', ['203.81.64.0/19', 
+    knownResults = ( ('mm', ['203.81.64.0/19',
                             '203.81.160.0/20']),
                     ('kp', ['175.45.176.0/22']))
 
