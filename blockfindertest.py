95ed12c472344992c7c4b20a4893ad154cc7c7d2
xiongchiamiov <xiong.chiamiov@gmail.com>
2014-03-08 09:12:51 -0800

    Fail tests on failed tests
    Travis CI uses exit codes to determine if tests pass.  Unfortunately, we
    haven't been setting the exit code, so even when tests fail, the return code
    has been a success.  Here's an example:
    
    https://travis-ci.org/ioerror/blockfinder/jobs/20337141
    
    Now we check to see if any of the tests failed - and if they did, indicate a
    failure by returning the number of failed tests.  This works because of bash's
    strange error handling - 0 is true (success), while 1-255 are false (failure).

--- blockfindertest.py
+++ blockfindertest.py
@@ -2,6 +2,7 @@
 import unittest
 import os
 import shutil
+import sys
 import tempfile
 
 import blockfinder
@@ -84,8 +85,11 @@ class CheckBlockFinder(BaseBlockfinderTest):
 
 
 if __name__ == '__main__':
+    failures = 0
     for test_class in [CheckReverseLookup, CheckBlockFinder]:
         test_suite = unittest.makeSuite(test_class)
         test_runner = unittest.TextTestRunner(verbosity=2)
-        test_runner.run(test_suite)
-
+        results = test_runner.run(test_suite)
+        failures += len(results.errors)
+        failures += len(results.failures)
+    sys.exit(failures)
